7e70dc5946dd919ed38fe3b7e5523b20f3e6d53c
OneManLegend <a1syed@ryerson.ca>
2022-08-07 23:09:39 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import StartGameScreen from './screens/StartGameScreen';0
+
+
+export default function App() {
+  return (
+    <StartGameScreen/>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
